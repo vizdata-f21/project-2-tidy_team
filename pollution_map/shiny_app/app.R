a18b31a65fdf9ebcd4ddd9febe9899620d3437a0
Courtney Lee <courtney.lee@duke.edu>
2021-11-15 13:03:49 -0500

    changes to shiny app

--- pollution_map/shiny_app/app.R
+++ pollution_map/shiny_app/app.R
@@ -22,41 +22,35 @@ library(shiny)
 library(rgdal)
 
 ui<- fluidPage(
-  #Assign Dasbhoard title
   titlePanel("Air pollution deaths"),
-
-  # Start:  the First Block
-  # Sliderinput: select from the date between 01.20.2020
-  # and 01.04.2020
-  sliderInput(inputId = "date", "Date:", min =
-                as.Date("1990"), max = as.Date("2017"),
+  # sliderInput(inputId = "date", "Date:",
+  #             min = as.Date("1990"), max = as.Date("2017"),
 
   # plot leaflet object (map)
-  leafletOutput(outputId = "distPlot", width = "700px",
-                height = "300px"),
-  #End:  the First Block
+  leafletOutput(outputId = "distPlot", width = "700px", height = "300px"),
 
-  #Start: the second Block
   sidebarLayout(
-
-    #Sidebar Panel: the selected country, history and
-    #whether to plot daily new confirmed cases.
     sidebarPanel(
       selectInput("air_pollution_type", h4("Type of Air pollution"), choices
-                  =list("Ambient Particular Matter","Household Solid Fuels","Ambient Ozone", "Total"), selected = "Ambient Particular Matter"),
+                  = list("Ambient Particular Matter","Household Solid Fuels",
+                         "Ambient Ozone", "Total"),
+                  selected = "Ambient Particular Matter"),
       selectInput("other_risk_factors", h4("Other Risk Factors"),
                   choices = list("Unsafe water Source", "Unsafe Sanitation",
-                                 "NO Access to hand wash","Non exclusive breast feeding"), selected = "Unsafe water Source")),
+                                 "No Access to hand wash",
+                                 "Non exclusive breast feeding"),
+                  selected = "Unsafe water Source")),
 
-    #Main Panel: plot the selected values
-    mainPanel (
-      plotOutput(outputId = "Plotcountry", width = "500px",
-                 height = "300px")
+    # Main Panel: plot the selected values
+    # mainPanel (
+    #   plotOutput(outputId = "Plotcountry", width = "500px",
+    #              height = "300px")
     )
-  ),
+  )
   #End: the second Block
-))
 
+
+shinyApp(ui = ui, server = server)
 
 
 
