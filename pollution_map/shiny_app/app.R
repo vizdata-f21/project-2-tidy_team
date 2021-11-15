043ed980495ef2760bbf956fe04b44f5f28d2697
raghav1369 <raghavrasal1@gmail.com>
2021-11-15 12:55:36 -0500

    first attempt at shiny map

--- pollution_map/shiny_app/app.R
+++ pollution_map/shiny_app/app.R
@@ -7,13 +7,56 @@
 #    http://shiny.rstudio.com/
 #
 
+# library(shiny)
+#
+# # Define UI for application that draws a histogram
+# ui <- fluidPage()
+#
+# # Define server logic required to draw a histogram
+# server <- function(input, output) {}
+#
+# # Run the application
+# shinyApp(ui = ui, server = server)
+
 library(shiny)
+library(rgdal)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage()
+ui<- fluidPage(
+  #Assign Dasbhoard title
+  titlePanel("Air pollution deaths"),
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {}
+  # Start:  the First Block
+  # Sliderinput: select from the date between 01.20.2020
+  # and 01.04.2020
+  sliderInput(inputId = "date", "Date:", min =
+                as.Date("1990"), max = as.Date("2017"),
 
-# Run the application
-shinyApp(ui = ui, server = server)
+  # plot leaflet object (map)
+  leafletOutput(outputId = "distPlot", width = "700px",
+                height = "300px"),
+  #End:  the First Block
+
+  #Start: the second Block
+  sidebarLayout(
+
+    #Sidebar Panel: the selected country, history and
+    #whether to plot daily new confirmed cases.
+    sidebarPanel(
+      selectInput("air_pollution_type", h4("Type of Air pollution"), choices
+                  =list("Ambient Particular Matter","Household Solid Fuels","Ambient Ozone", "Total"), selected = "Ambient Particular Matter"),
+      selectInput("other_risk_factors", h4("Other Risk Factors"),
+                  choices = list("Unsafe water Source", "Unsafe Sanitation",
+                                 "NO Access to hand wash","Non exclusive breast feeding"), selected = "Unsafe water Source")),
+
+    #Main Panel: plot the selected values
+    mainPanel (
+      plotOutput(outputId = "Plotcountry", width = "500px",
+                 height = "300px")
+    )
+  ),
+  #End: the second Block
+))
+
+
+
+
