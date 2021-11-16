0efa3e31c3ee74f065d434634f04075ad3b78d66
raghav1369 <raghavrasal1@gmail.com>
2021-11-16 15:24:00 -0500

    tried implementing leaflet

--- pollution_map/shiny_app/app.R
+++ pollution_map/shiny_app/app.R
@@ -1,179 +1,166 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-# library(shiny)
-#
-# # Define UI for application that draws a histogram
-# ui <- fluidPage()
-#
-# # Define server logic required to draw a histogram
-# server <- function(input, output) {}
-#
-# # Run the application
-# shinyApp(ui = ui, server = server)
-
 library(shiny)
-library(rgdal)
-library(leaflet.minicharts)
-library(leaflet)
+library(tidyverse)
+library(spData) # For getting spatial data
+library(sf) # For preserving spatial data
+library(leaflet) # For making maps
+library(DT) # For making fancy tables
 
+df = subset(total_joined, select = -c(long,lat,group,order,subregion,code))
 
-newdata <- total_joined[c(5,8,9)]
-
-# newdata2 <- newdata %>%
-#   pivot_wider(
-#     names_from = region,
-#     values_from = death_rate_air_pollution
-#   )
-
-
-newdata_coord<-total_joined[c(1,2,5)]
-
-newdata_coord <- unique(newdata_coord)
-
-
-newdata2 <- newdata %>%
-  distinct() %>%
-  pivot_wider(., names_from = region, values_from = death_rate_air_pollution)
-
-newdata2 <- newdata2[-1, ]
-# # Remove countries that have ALL NAs
-# newdata2 <- newdata2[,colSums(is.na(newdata2))<nrow(newdata2)]
-
-ui<- fluidPage(
-  titlePanel("Air pollution deaths"),
-  sliderInput(inputId = "date", "Date:",
-              min = as.Date("1990"), max = as.Date("2017"),
-
-  # plot leaflet object (map)
-  leafletOutput(outputId = "distPlot", width = "700px", height = "300px"),
-
-  # sidebarLayout(
-  #   sidebarPanel(
-  #     selectInput("air_pollution_type", h4("Type of Air pollution"), choices
-  #                 = list("Ambient Particular Matter","Household Solid Fuels",
-  #                        "Ambient Ozone", "Total"),
-  #                 selected = "Ambient Particular Matter"),
-  #     selectInput("other_risk_factors", h4("Other Risk Factors"),
-  #                 choices = list("Unsafe water Source", "Unsafe Sanitation",
-  #                                "No Access to hand wash",
-  #                                "Non exclusive breast feeding"),
-  #                 selected = "Unsafe water Source")),
-
-    # Main Panel: plot the selected values
-    # mainPanel (
-    #   plotOutput(outputId = "Plotcountry", width = "500px",
-    #              height = "300px")
-    )
+df2<-df %>%
+  pivot_longer(
+    cols = -c(year,region),
+    names_to = "pollution_type",
+    values_to = "death_rate"
   )
-  #End: the second Block
+
+mapData <- world[c(2,11)]
+
+# Clean review data
+df2<-df2 %>%
+  drop_na(death_rate)
+df2$death_rate <- as.numeric(as.character(df2$death_rate))
+
+# Clean country data
 
 
-server <- function(input, output){
+# Define UI for application
+ui <- fluidPage(
 
-  #Assign output$distPlot with renderLeaflet object
-  output$distPlot <- renderLeaflet({
+  # CSS
+  tags$head(
+    tags$style(HTML("
+      body { background-color: #f2efe9; }
+      .container-fluid { background-color: #fff; width: 1100px; padding: 60px; }
+      .topimg { width: 120px; display: block; margin: 0px auto 40px auto; }
+      .title { text-align: center; }
+      .toprow { margin: 60px 0px; padding: 30px; background-color: #fae8bb; }
+      .filters { margin: 0px auto; }
+      .shiny-input-container { width:100% !important; }
+      .table { padding: 30px; margin-top: 30px; }
+      .leaflet-top { z-index:999 !important; }
+      "))
+  ),
 
-    # row index of the selected date (from input$date)
-    rowindex = which(as.Date(as.character(newdata2$year),
-                             "%Y") ==input$date)
 
-    # initialise the leaflet object
-    basemap= leaflet()  %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE))
+  # Application title
+  h1("Air Pollution Stats", class = "title"),
 
-    # assign the chart colors for each country, where those
-    # countries with more than 500,000 cases are marked
-    # as red, otherwise black
-    chartcolors = rep("black",7)
-    stresscountries = which(as.numeric(daten[rowindex,c(2:8)])>50000)
-    chartcolors[stresscountries]= rep("red", length(stresscountries))
+  fluidRow(class = "toprow",
+           fluidRow (class = "filters",
 
-    # add chart for each country according to the number of
-    # confirmed cases to selected date
-    # and the above assigned colors
-    basemap %>%
-      addMinicharts(
-        citydaten$long, citydaten$Lat,
-        chartdata = as.numeric(daten[rowindex,c(2:8)]),
-        showLabels = TRUE,
-        fillColor = chartcolors,
-        labelMinSize = 5,
-        width = 45,
-        transitionTime = 1
-      )
+                     column(6,
+                            # Style Menu
+                            selectInput("style", "Style", c("All",
+                                                            "Bowl",
+                                                            "Box",
+                                                            "Cup",
+                                                            "Pack",
+                                                            "Tray"))
+                     ),
+
+                     column(6,
+                            # Country menu
+                            selectInput("country", "Country", levels(ratings$Country) %>%
+                                          append("All") %>% # Add "All" option
+                                          sort()) # Sort options alphabetically
+
+                     )
+           )
+  ),
+
+  fluidRow (
+
+    column(6, class = "map",
+           # Map
+           leafletOutput("map")
+    )
+
+  )
+)
+
+# Define server logic
+server <- function(input, output) {
+
+  # Create world map
+  output$map <- renderLeaflet({
+
+    # Filter data based on selected Style
+    if (input$style != "All") {
+      ratings <- filter(ratings, Style == input$style)
+    }
+
+    # Filter data based on selected Country
+    if (input$country != "All") {
+      ratings <- filter(ratings, Country == input$country)
+    }
+
+    # Hide map when user has filtered out all data
+    validate (
+      need(nrow(ratings) > 0, "")
+    )
+
+    # Get average rating by country
+    countries <- group_by(ratings, Country) %>%
+      summarise(avgRating = mean(Stars))
+
+    # Add spatial data to countries dataframe
+    countries <- left_join(countries, mapData, c("Country" = "name_long"))
+
+    # Create color palette for map
+    pal <- colorNumeric(palette = "YlOrRd", domain = countries$avgRating)
+
+    # Create label text for map
+    map_labels <- paste("Ramen from",
+                        countries$Country,
+                        "has an average rating of",
+                        round(countries$avgRating, 1))
+
+    # Generate basemap
+    map <- leaflet() %>%
+      addTiles() %>%
+      setView(0, 0, 1)
+
+    # Add polygons to map
+    map %>% addPolygons(data = countries$geom,
+                        fillColor = pal(countries$avgRating),
+                        fillOpacity = .7,
+                        color = "grey",
+                        weight = 1,
+                        label = map_labels,
+                        labelOptions = labelOptions(textsize = "12px")) %>%
+
+      # Add legend to map
+      addLegend(pal = pal,
+                values = countries$avgRating,
+                position = "bottomleft")
+
   })
 
-  #Assign output$Plotcountry with renderPlot object
-  output$Plotcountry <- renderPlot({
+  # Create data table
+  output$table <- renderDataTable({
 
-    #the selected country
-    chosencountry = input$selectedcountry
-
-    #assign actual date
-    today = as.Date("2020/04/02")
-
-    #size of the selected historic window
-    chosenwindow = input$selectedhistoricwindow
-    if (chosenwindow == "the past 10 days")
-    {pastdays = 10}
-    if (chosenwindow  == "the past 20 days")
-    {pastdays = 20}
-
-    #assign the dates of the selected historic window
-    startday = today-pastdays-1
-    daten$Date=as.Date(as.character(daten$Date),"%d.%m.%Y")
-    selecteddata
-    = daten[(daten$Date>startday)&(daten$Date<(today+1)),
-            c("Date",chosencountry)]
-
-    #assign the upperbound of the y-aches (maximum+100)
-    upperboundylim = max(selecteddata[,2])+100
-
-    #the case if the daily new confirmed cases are also
-    #plotted
-    if (input$dailynew == TRUE){
-
-      plot(selecteddata$Date, selecteddata[,2], type = "b",
-           col = "blue", xlab = "Date",
-           ylab = "number of infected people", lwd = 3,
-           ylim = c(0, upperboundylim))
-      par(new = TRUE)
-      plot(selecteddata$Date, c(0, diff(selecteddata[,2])),
-           type = "b", col = "red", xlab = "", ylab =
-             "", lwd = 3,ylim = c(0,upperboundylim))
-
-      #add legend
-      legend(selecteddata$Date[1], upperboundylim*0.95,
-             legend=c("Daily new", "Total number"),
-             col=c("red", "blue"), lty = c(1,1), cex=1)
+    # Filter data based on selected Style
+    if (input$style != "All") {
+      ratings <- filter(ratings, Style == input$style)
     }
 
-    #the case if the daily new confirmed cases are
-    #not plotted
-    if (input$dailynew == FALSE){
-
-      plot(selecteddata$Date, selecteddata[,2], type = "b",
-           col = "blue", xlab = "Date",
-           ylab = "number of infected people", lwd = 3,
-           ylim = c(0, upperboundylim))
-      par(new = TRUE)
-
-      #add legend
-      legend(selecteddata$Date[1], upperboundylim*0.95,
-             legend=c("Total number"), col=c("blue"),
-             lty = c(1), cex=1)
+    # Filter data based on selected Country
+    if (input$country != "All") {
+      ratings <- filter(ratings, Country == input$country)
     }
+
+    # Hide table when user has filtered out all data
+    validate (
+      need(nrow(ratings) > 0, "")
+    )
+
+    ratings[,2:6]
 
   })
 
 }
 
+# Run the application
+shinyApp(ui = ui, server = server)
 
