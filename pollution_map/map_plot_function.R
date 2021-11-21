458c0f744d1389bd600b4d99f1474f2f43285553
Kathryn Thomas <kathryn.thomas@duke.edu>
2021-11-21 17:58:58 -0500

    starting working on plot function

---
+++ pollution_map/map_plot_function.R
@@ -0,0 +1,32 @@
+# Map Plot Function
+
+make_map <- function(data,
+                     fill) {
+  geom_polygon_interactive(
+  aes_string(group = "group", fill = fill), # need to figure out data + fill
+  color = "black", size = 0.3
+) +
+  coord_map(
+    projection = "mercator",
+    xlim = c(-180, 180)
+  ) +
+  scale_fill_viridis_c(
+    option = "inferno",
+    name = "Death rate",
+    labels = label_number(big.mark = ","),
+    na.value = "lightgray"
+  ) +
+  theme_void() +
+  theme(
+    text = element_text(color = "black"),
+    legend.direction = "vertical",
+    legend.position = "left",
+    legend.key.height = unit(2, "cm"),
+    plot.background = element_rect(fill = "white", color = "white"),
+    plot.title = element_blank(),
+    plot.subtitle = element_blank()
+  )
+}
+
+
+make_map(data = total_joined, fill = death_rate_air_pollution)
